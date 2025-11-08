0f2f16225034fce00c664749296477a182bf04b9
Isaac Wood <20ipw@queensu.ca>
2025-11-08 17:49:14 -0500

    initialized

---
+++ Jumble/Jumble/jumble.cpp
@@ -0,0 +1,10 @@
+//
+//  jumble.cpp
+//  Jumble
+//
+//  Created by Isaac W on 2025-11-08.
+//
+#include "jumble.h"
+JumblePuzzle::JumblePuzzle(const std::string& hidden, const std::string& difficulty) {
+    
+}
---
+++ Jumble/Jumble/jumble.h
@@ -0,0 +1,18 @@
+//
+//  jumble.h
+//  Jumble
+//
+//  Created by Isaac W on 2025-11-08.
+//
+#include <string>
+#include <iostream>
+#include <ctime>
+class JumblePuzzle {
+private:
+
+    
+    
+    public:
+    JumblePuzzle(const std::string& hidden, const std::string& difficulty);
+    JumblePuzzle(JumblePuzzle& jb);
+}
